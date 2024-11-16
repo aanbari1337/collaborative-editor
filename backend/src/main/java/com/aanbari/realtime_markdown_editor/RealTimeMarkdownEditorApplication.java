a26e321461a167c2e2c47f4d236c46490277666c
aanbari1337 <amineanbari122@gmail.com>
2024-11-16 20:01:07 +0100

    first commit

---
+++ backend/src/main/java/com/aanbari/realtime_markdown_editor/RealTimeMarkdownEditorApplication.java
@@ -0,0 +1,13 @@
+package com.aanbari.realtime_markdown_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeMarkdownEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeMarkdownEditorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aanbari/realtime_markdown_editor/RealTimeMarkdownEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aanbari.realtime_markdown_editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeMarkdownEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
