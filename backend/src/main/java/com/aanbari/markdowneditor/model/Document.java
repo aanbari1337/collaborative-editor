a85ba5bfe322ad6d694e1ba7c9c1169510f2f6d7
aanbari1337 <amineanbari122@gmail.com>
2024-11-17 13:03:56 +0100

    feat: document entity

---
+++ backend/src/main/java/com/aanbari/markdowneditor/model/Document.java
@@ -0,0 +1,33 @@
+package com.aanbari.markdowneditor.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public Document() {
+    }
+
+    public Document( String content, LocalDateTime createdAt) {
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+
+}
