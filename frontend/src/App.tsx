b1a5310e496512171e323907bfe867048d9c4673
aanbari1337 <amineanbari122@gmail.com>
2024-11-18 14:46:21 +0100

    feat: react-query and axios instance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import AppRoutes from "./routes";
 import AppLayout from "./components/app-layout";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
-      <AppLayout>
-        <AppRoutes />
-      </AppLayout>
+      <QueryClientProvider client={queryClient}>
+        <AppLayout>
+          <AppRoutes />
+        </AppLayout>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
