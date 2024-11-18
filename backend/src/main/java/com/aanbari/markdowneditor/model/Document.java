148b82811e12b1c47e0f4b4b1e1a367a95e589ce
aanbari1337 <amineanbari122@gmail.com>
2024-11-18 12:22:34 +0100

    feat: add document title

--- backend/src/main/java/com/aanbari/markdowneditor/model/Document.java
+++ backend/src/main/java/com/aanbari/markdowneditor/model/Document.java
@@ -18,6 +18,7 @@ public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
+    private String title;
     private String content;
     private LocalDateTime createdAt;
 
