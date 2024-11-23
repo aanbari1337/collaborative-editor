f93bf20fa756f6afc118f8c48a2b688c794b7d71
Amine Anbari <amine.a@wiggli.io>
2024-11-23 14:01:42 +0100

    delete document

--- frontend/src/components/document-component.tsx
+++ frontend/src/components/document-component.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import TrashIcon from "@/assets/icons/trash-icon.svg?react";
+import useDeleteDocument from "@/pages/document/api/delete-document";
 
 interface Props {
   id: number;
@@ -7,12 +9,24 @@ interface Props {
   createdAt: string;
 }
 const DocumentComponent = ({ id, title, createdAt }: Props) => {
+  const { mutate: deleteDocument } = useDeleteDocument({ id });
+
+  const onDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    deleteDocument();
+  };
   return (
-    <Link to={`/documents/${id}`}>
-      <div className='font-fira p-2 border-b border-b-gray-200 cursor-pointer'>
+    <Link
+      to={`/documents/${id}`}
+      className='flex justify-between items-center p-2 border-b border-b-gray-200 font-fira '
+    >
+      <div className='flex flex-col'>
         <h3 className='text-base font-semibold'>{title}</h3>
         <span className='text-xs text-gray-500'>{createdAt}</span>
       </div>
+      <button className='text-gray-500' onClick={onDelete}>
+        <TrashIcon width={20} height={20} />
+      </button>
     </Link>
   );
 };
---
+++ frontend/src/pages/document/api/delete-document.ts
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "react-query";
+import API from "@/api/axios-instance";
+import { GET_DOCUMENT_LIST_KEY } from "../../../api/get-documents";
+
+const useDocument = ({ id }: { id: number }) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (body: { title: string; content: string }) => {
+      return API.put(`/documents/document/${id}`, body);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(GET_DOCUMENT_LIST_KEY);
+    },
+  });
+};
+
+export default useDocument;
